5777c129d0c39e74a5a22fb5a77d7a37d2bb6312
Sahil Kumar <148254352+sahil-kumar-17@users.noreply.github.com>
2026-06-28 22:13:48 +0530

    Time: 0 ms (100%), Space: 42.1 MB (33.99%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int tribonacci(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 1;
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
+    }
+
+    private int helper(int n, int[] dp) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 1;
+        if (n >= 0 && dp[n] != -1) {
+            return dp[n];
+        }
+        return dp[n] = helper(n - 1, dp) + helper(n - 2, dp) + helper(n - 3, dp);
+    }
+}
