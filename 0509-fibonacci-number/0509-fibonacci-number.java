246433934460097af426ff0da1524c083984223d
Sahil Kumar <148254352+sahil-kumar-17@users.noreply.github.com>
2026-07-01 11:46:17 +0530

    Time: 0 ms (100%), Space: 42 MB (51.78%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,13 @@
 class Solution {
     public int fib(int n) {
-       if(n==0)return 0;
-       if (n==1)return 1;
-       return fib(n-1)+fib(n-2); 
+        if(n<2)return n;
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(n, dp);
+    }
+    public int helper(int n,int[]dp){
+        if(n<2)return n;
+        if(dp[n]!=-1)return dp[n];
+        return dp[n]=helper(n-1,dp)+helper(n-2,dp);
     }
 }
