40681a66c032e441b74d6f22333da1aba1f666cc
Sahil Kumar <148254352+sahil-kumar-17@users.noreply.github.com>
2026-07-06 11:03:00 +0530

    Time: 1 ms (99.8%), Space: 45.5 MB (49.96%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,19 +1,24 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n=triangle.size();
-        int dp[][]=new int[n][n];
-        dp[0][0]=triangle.get(0).get(0);
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=i;j++){
-                int p1=i-1>-1&&j<=i-1?dp[i-1][j]:(int)1e9;
-                int p2=i-1>-1&&j-1>-1?dp[i-1][j-1]:(int)1e9;
-                dp[i][j]=triangle.get(i).get(j)+Math.min(p1,p2);
+        int[][]dp=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
             }
         }
-        int minsum=(int)1e9;
-        for(int i=0;i<n;i++){
-            minsum=Math.min(minsum,dp[n-1][i]);
+        dp[0][0]=triangle.get(0).get(0);
+        int ans=(int)1e9;
+        for(int j=0;j<n;j++){
+        ans=Math.min(ans,helper(dp,n-1,j,triangle));
         }
-        return minsum;
+        return ans;
+    }
+    private int helper(int[][]dp,int i,int j,List<List<Integer>>triangle){
+       if(i==0&&j==0)return triangle.get(0).get(0);
+       if(dp[i][j]!=-1)return dp[i][j];
+        int p1=j>0?helper(dp,i-1,j-1,triangle):(int)1e9;
+        int p2=j<i?helper(dp,i-1,j,triangle):(int)1e9;
+        return dp[i][j]=triangle.get(i).get(j)+Math.min(p1,p2);
     }
 }
