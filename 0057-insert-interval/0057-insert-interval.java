205696715ad208c315141e40cef1d213dd78f641
Sahil Kumar <148254352+sahil-kumar-17@users.noreply.github.com>
2026-08-06 22:51:07 +0530

    Time: 2 ms (26.05%), Space: 47 MB (57.44%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        if(intervals.length==0){
+            int ans[][]={{newInterval[0],newInterval[1]}};
+            return ans;
+        }
+        int makeShift[][]=new int[intervals.length+1][intervals[0].length];
+        for(int i=0;i<intervals.length;i++){
+            for(int j=0;j<intervals[0].length;j++){
+                makeShift[i][j]=intervals[i][j];
+            }
+        }
+        makeShift[intervals.length]=newInterval;
+        Arrays.sort(makeShift,(a,b)->a[0]-b[0]);
+
+        List<int[]>mylist=new ArrayList<>();
+        int []curr=makeShift[0];
+        for(int i=1;i<makeShift.length;i++){
+            if(curr[1]>=makeShift[i][0]){
+                curr[1]=Math.max(curr[1],makeShift[i][1]);
+            }else{
+                mylist.add(curr);
+                curr=makeShift[i];
+            }
+        }
+        mylist.add(curr);
+        return mylist.toArray(new int [mylist.size()][]);
+    }
+}
