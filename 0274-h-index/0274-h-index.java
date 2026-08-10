61b93098b376bce9bd33a0c883a60abe964aecd5
Sahil Kumar <148254352+sahil-kumar-17@users.noreply.github.com>
2026-08-10 21:28:32 +0530

    Time: 5 ms (73.67%), Space: 43.8 MB (5.74%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,19 +1,12 @@
 class Solution {
     public int hIndex(int[] nums) {
         Arrays.sort(nums);
-        int n=nums.length;
-        int count=0;
-        for(int i=0;i<n;i++){
-            int temp=0;
-            int page=nums[i];
-            for(int j=i;j<n;j++){
-                if(page>0&&nums[j]>=nums[i]){
-                    temp++;
-                    page--;
-                }
-                count=Math.max(count,temp);
+        for (int i = 0; i < nums.length; i++) {
+            int pages = nums.length - i;
+            if (nums[i] >= pages) {
+                return pages;
             }
         }
-        return count;
+        return 0;
     }
 }
