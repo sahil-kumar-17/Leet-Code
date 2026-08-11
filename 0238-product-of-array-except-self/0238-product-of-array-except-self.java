1e4f7e879b8f343c7d20ec72af71449116dfebbc
Sahil Kumar <148254352+sahil-kumar-17@users.noreply.github.com>
2026-08-11 22:18:44 +0530

    Time: 2 ms (94.01%), Space: 72.1 MB (28.9%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        if (nums.length == 1)
+            return nums;
+        int ans[] = new int[nums.length];
+        ans[0]=1;
+        for(int i=1;i<nums.length;i++){
+            ans[i]=ans[i-1]*nums[i-1];
+        }
+        int suffix=1;
+        for(int i=nums.length-1;i>=0;i--){
+            ans[i]*=suffix;
+            suffix*=nums[i];
+        }
+        return ans;
+    }
+
+}
