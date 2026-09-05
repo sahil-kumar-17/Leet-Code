325d6101704096cfa6b9d792f680200814c0fd1b
Sahil Kumar <148254352+sahil-kumar-17@users.noreply.github.com>
2026-09-05 18:26:44 +0530

    Time: 0 ms (100%), Space: 42.8 MB (26.44%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,21 +1,17 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        List<Integer> temp = new ArrayList<>();
-        return helper(0, 0, 0, temp, nums);
+        return helper(0, 0, 0, nums);
     }
 
-    private int helper(int i, int xor, int sum, List<Integer> temp, int[] nums) {
-        if (i > nums.length - 1) {
-            for (int j : temp) {
-                xor ^= j;
-            }
+    private int helper(int i, int xor, int sum, int[] nums) {
+        if (i > nums.length - 1) {            
             sum += xor;
             return sum;
         }
-        temp.add(nums[i]);
-        int pick=helper(i+1,xor,sum,temp,nums);
-        temp.remove(temp.size()-1);
-        int notpick=helper(i+1,xor,sum,temp,nums);
+        xor^=nums[i];
+        int pick=helper(i+1,xor,sum,nums);
+        xor^=nums[i];
+        int notpick=helper(i+1,xor,sum,nums);
         sum=pick+notpick;
         return sum;
     }
