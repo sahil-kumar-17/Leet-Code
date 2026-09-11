c758bb0b2596a2bc650a7dc22fad9fc1ca31fab7
Sahil Kumar <148254352+sahil-kumar-17@users.noreply.github.com>
2026-09-11 19:05:22 +0530

    Time: 6 ms (50.4%), Space: 46.2 MB (58.53%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.java
@@ -1,33 +1,20 @@
 class Solution {
     public int totalNumbers(int[] digits) {
-        HashSet<Integer> myset = new HashSet<>();
-        boolean[] used = new boolean[digits.length];
-        for (int i = 0; i < digits.length; i++) {
+        HashSet<Integer>myset=new HashSet<>();
+        for(int i=0;i<digits.length;i++){
             if(digits[i]==0)continue;
-            used[i] = true;
-            helper( 1, digits[i], used, digits, myset);
-            used[i] = false;
-        }
-        return myset.size();
-    }
-
-    private int helper(int d, int num, boolean[] used, int[] digits, HashSet<Integer> myset) {
-        if (d == 3) {
-            if ( num % 2 == 0) {
-                myset.add(num);
-            }
-            return myset.size();
-        }
-        for (int i = 0; i < digits.length; i++) {
-            if (!used[i]) {
-            int newnum=0;
-                newnum = (num * 10) + digits[i];
-                used[i] = true;
-                helper( d + 1, newnum, used, digits, myset);
-                used[i] = false;
+            for(int j=0;j<digits.length;j++){
+                for(int k=0;k<digits.length;k++){
+                    if(i==j||i==k||k==i||k==j){
+                        continue;
+                    }
+                    int num=digits[i]*100+digits[j]*10+digits[k];
+                    if(num%2==0){
+                        myset.add(num);
+                    }
+                }
             }
         }
-
         return myset.size();
     }
 }
